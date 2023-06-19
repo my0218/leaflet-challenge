5c30d8ed6c860aa13342d8acc2bc45c7bec59a6e
my0218 <124230467+my0218@users.noreply.github.com>
2023-06-18 20:02:03 -0400

    version2

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -9,44 +9,42 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Fetch the earthquake data
-fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    data.features.forEach(function (earthquake) {
-      var latitude = earthquake.geometry.coordinates[1];
-      var longitude = earthquake.geometry.coordinates[0];
+// Get earchquake data
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+d3.json(url).then(function (data) {
+  data.features.forEach(function (earthquake) {
+    var latitude = earthquake.geometry.coordinates[1];
+    var longitude = earthquake.geometry.coordinates[0];
 
-      var magnitude = earthquake.properties.mag;
-      var depth = earthquake.geometry.coordinates[2];
+    var magnitude = earthquake.properties.mag;
+    var depth = earthquake.geometry.coordinates[2];
 
-      var markerSize = Math.pow(2, magnitude) * 2;
-      var markerColor = getColor(depth);
+    var markerSize = Math.pow(2, magnitude) * 2;
+    var markerColor = getColor(depth);
 
-      var marker = L.circleMarker([latitude, longitude], {
-        radius: markerSize,
-        fillColor: markerColor,
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-      }).addTo(myMap);
+    var marker = L.circleMarker([latitude, longitude], {
+      radius: markerSize,
+      fillColor: markerColor,
+      color: '#000',
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    }).addTo(myMap);
 
-      // Create popup content
-      var popupContent = "<b>Magnitude:</b> " + magnitude + "<br>" +
-        "<b>Location:</b> " + latitude + ", " + longitude + "<br>" +
-        "<b>Depth:</b> " + depth + " km";
-      marker.bindPopup(popupContent);
-    });
+    // Create popup content
+    var popupContent = "<b>Magnitude:</b> " + magnitude + "<br>" +
+      "<b>Location:</b> " + latitude + ", " + longitude + "<br>" +
+      "<b>Depth:</b> " + depth + " km";
+    marker.bindPopup(popupContent);
   });
-
+}).catch(function (error) {
+  console.log("Error loading earthquake data:", error);
+});
 
 // Calculate marker color based on depth
 function getColor(depth) {
-  var colors = ['#00ff00', '#ffff00', '#ffa500', '#ff7f00', '#ff4500', '#ff0000']; 
-  var depthRanges = [10, 30, 50, 70, 90]; 
+  var colors = ['#00ff00', '#ffff00', '#ffa500', '#ff7f00', '#ff4500', '#ff0000'];
+  var depthRanges = [10, 30, 50, 70, 90];
 
   for (var i = 0; i < depthRanges.length; i++) {
     if (depth <= depthRanges[i]) {
@@ -54,7 +52,7 @@ function getColor(depth) {
     }
   }
 
-  return colors[colors.length - 1]; 
+  return colors[colors.length - 1];
 }
 
 // Create the legend control
